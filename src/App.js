6c5074e95fbe84c40b18c742aa0aec56fe435d2e
Githinji Mugambi <ggathungu20905@gmail.com>
2024-05-06 12:08:18 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 
-import { useEffect,useState } from 'react';
+
 import './App.css';
-import { useParams } from 'react-router-dom';
-import { getbot } from './Utilities';
+
 import Displaybot from './components/Displaybots';
-import { Outlet,useNavigate } from 'react-router-dom';
+
 
 function App() {
 
